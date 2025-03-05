5e6fda0a4a9d6c2adc90e8d27c2a794fe7730c5f
Santiago Hernandez <santiagohb2830@gmail.com>
2025-03-05 14:26:34 -0500

    Update arbolBinario.cpp

--- EDD_laboratorio03/arbolBinario.cpp
+++ EDD_laboratorio03/arbolBinario.cpp
@@ -2,37 +2,37 @@
 
 // Nueva creación de nodo
 struct node *newNode(int data) {
-    struct node *node = (struct node *)malloc(sizeof(struct node));
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
+    struct node *node = (struct node *)malloc(sizeof(struct node)); // Asignar memoria para el nodo
+    node->data = data;  // Asignar el dato al nodo
+    node->left = NULL;   // Inicializar el hijo izquierdo como NULL
+    node->right = NULL;  // Inicializar el hijo derecho como NULL
+    return node;         // Retornar el nodo creado
 }
 
 // Recorrido Preorden
 void traversePreOrder(struct node *temp) {
     if (temp != NULL) {
-        cout << " " << temp->data;
-        traversePreOrder(temp->left);
-        traversePreOrder(temp->right);
+        cout << " " << temp->data;  // Imprimir el dato del nodo actual
+        traversePreOrder(temp->left);  // Recorrer el hijo izquierdo
+        traversePreOrder(temp->right); // Recorrer el hijo derecho
     }
 }
 
 // Recorrido Inorden
 void traverseInOrder(struct node *temp) {
     if (temp != NULL) {
-        traverseInOrder(temp->left);
-        cout << " " << temp->data;
-        traverseInOrder(temp->right);
+        traverseInOrder(temp->left);  // Recorrer el hijo izquierdo
+        cout << " " << temp->data;  // Imprimir el dato del nodo actual
+        traverseInOrder(temp->right); // Recorrer el hijo derecho
     }
 }
 
 // Recorrido Postorden
 void traversePostOrder(struct node *temp) {
     if (temp != NULL) {
-        traversePostOrder(temp->left);
-        traversePostOrder(temp->right);
-        cout << " " << temp->data;
+        traversePostOrder(temp->left);  // Recorrer el hijo izquierdo
+        traversePostOrder(temp->right); // Recorrer el hijo derecho
+        cout << " " << temp->data;  // Imprimir el dato del nodo actual
     }
 }
 
